2bf7877a3df519f685529bc733e27a4d795804fa
Anaid0616 <152166423+Anaid0616@users.noreply.github.com>
2025-01-22 17:38:05 +0100

    Styled register and login forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
 module.exports = {
   content: ['./**/*.{html,js,ts,mjs}', '!./node_modules/**/*'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
